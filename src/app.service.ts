e01cc09ee9b179dccc3ecae47be98c6839dc79f7
Felipe Lima <felipedlimacontato@gmail.com>
2024-11-23 16:59:50 -0300

    update hello

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!'
+    return 'Olá Mundo! Pera aí... O que você está fazendo aqui??'
   }
 }
