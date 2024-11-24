8ad764da8ef72f88ddf4994c22b26aaa467ec8f5
Felipe Lima <felipedlimacontato@gmail.com>
2024-11-24 11:59:35 -0300

    add plain to instance

--- src/client/establishments/establishments.controller.ts
+++ src/client/establishments/establishments.controller.ts
@@ -1,21 +1,31 @@
 import { Controller, Get, Param, Query } from '@nestjs/common'
 import { EstablishmentsService } from './establishments.service'
+import { plainToInstance } from 'class-transformer'
+import { EstablishmentEntity } from './entities/establishment.entity'
 
 @Controller('public/establishments')
 export class EstablishmentsController {
   constructor(private readonly establishmentsService: EstablishmentsService) {}
 
   @Get()
-  findAll(
+  async findAll(
     @Query('search') search: string,
     @Query('page') page: string,
     @Query('limit') limit: string
   ) {
-    return this.establishmentsService.findAll(search, +page || 1, +limit || 10)
+    const establishments = await this.establishmentsService.findAll(
+      search,
+      +page || 1,
+      +limit || 10
+    )
+
+    return plainToInstance(EstablishmentEntity, establishments)
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.establishmentsService.findOne(id)
+  async findOne(@Param('id') id: string) {
+    const establishment = await this.establishmentsService.findOne(id)
+
+    return plainToInstance(EstablishmentEntity, establishment)
   }
 }
